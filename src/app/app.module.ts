d0ebfeac0ab00919f4d5a94b7431a2181fb419b9
JRicardoP <ricardo.pachecoescobar@gmail.com>
2022-04-03 10:49:04 -0500

    Creando los componentes del proyecto

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SobremiComponent } from './components/sobremi/sobremi.component';
+import { AboutComponent } from './components/about/about.component';
+import { ProjectsComponent } from './components/projects/projects.component';
+import { CreateprojectsComponent } from './components/createprojects/createprojects.component';
+import { CreateprojectComponent } from './components/createproject/createproject.component';
+import { ContactComponent } from './components/contact/contact.component';
+import { EditprojectComponent } from './components/editproject/editproject.component';
+import { ListprojectComponent } from './components/listproject/listproject.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SobremiComponent,
+    AboutComponent,
+    ProjectsComponent,
+    CreateprojectsComponent,
+    CreateprojectComponent,
+    ContactComponent,
+    EditprojectComponent,
+    ListprojectComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/listproject/listproject.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-listproject',
+  templateUrl: './listproject.component.html',
+  styleUrls: ['./listproject.component.css']
+})
+export class ListprojectComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
